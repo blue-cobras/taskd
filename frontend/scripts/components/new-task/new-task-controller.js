579ce08b656ffd274ddadd86d3b2325a0106c261
Chad Hines <chad@newlanguagestudio.com>
2016-06-23 17:22:41 -0500

    update to the mock-up for todo list
    front end functionality

--- frontend/scripts/components/new-task/new-task-controller.js
+++ frontend/scripts/components/new-task/new-task-controller.js
@@ -8,15 +8,20 @@
 		// Vars
 		var vm = this
 		vm.createTask = createTask
+		vm.removeTask = removeTask
 		vm.newTask = {}
-
+		vm.tasks=[]
 		// Functions
 		function createTask() {
 			// TODO POST to back-end
+			vm.tasks.push(vm.newTask)
 			// Reset model
 			vm.newTask = {}
 		}
-
+		function removeTask(i) {
+			vm.tasks.splice(i,1)
+		}
 	}
 
 })()
+
