cae4cbc001edd3195324bb0512c2953dec3d256d
McSkwid <zmixon91@gmail.com>
2016-06-25 05:58:09 -0600

    add datepicker to main view and fix main view CSS

--- frontend/scripts/components/new-task/new-task-controller.js
+++ frontend/scripts/components/new-task/new-task-controller.js
@@ -11,12 +11,89 @@
 		// vm.removeTask = removeTask
 		vm.newTask = {}
 		vm.tasks = []
+
+		//START DATEPICKER
+		// Vars
+		let tomorrow = new Date()
+		let afterTomorrow = new Date()
+		tomorrow.setDate(tomorrow.getDate() + 1)
+		afterTomorrow.setDate(tomorrow.getDate() + 1)
+		vm.inlineOptions = {
+			customClass: getDayClass,
+			minDate: new Date(),
+			showWeeks: true
+		}
+		vm.dateOptions = {
+			formatYear: 'yy',
+			maxDate: new Date(2020, 5, 22),
+			minDate: new Date(),
+			startingDay: 1
+		}
+		vm.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate']
+		vm.format = vm.formats[0]
+		vm.altInputFormats = ['M!/d!/yyyy']
+		vm.popup2 = {
+			opened: false
+		}
+		vm.events = [
+			{
+				date: tomorrow,
+				status: 'full'
+			},
+			{
+				date: afterTomorrow,
+				status: 'partially'
+			}
+		]
+		// Exports
+		vm.today = today
+		vm.clear = clear
+		vm.toggleMin = toggleMin
+		vm.setDate = setDate
+		vm.open2 = open2
+		// Run
+		vm.today()
+		vm.toggleMin()
+		// Functions
+		function getDayClass(data) {
+			let date = data.date,
+				mode = data.mode
+			if (mode === 'day') {
+				let dayToCheck = new Date(date).setHours(0, 0, 0, 0)
+				for (let i = 0; i < vm.events.length; i++) {
+					let currentDay = new Date(vm.events[i].date).setHours(0, 0, 0, 0)
+					if (dayToCheck === currentDay) {
+						return vm.events[i].status
+					}
+				}
+			}
+			return ''
+		}
+		function today() {
+			vm.newTask.due = new Date()
+		}
+		function clear() {
+			vm.newTask.due = null
+		}
+		function toggleMin() {
+			vm.inlineOptions.minDate = vm.inlineOptions.minDate ? null : new Date()
+			vm.dateOptions.minDate = vm.inlineOptions.minDate
+		}
+		function setDate(year, month, day) {
+			vm.newTask.due = new Date(year, month, day)
+		}
+		function open2() {
+			vm.popup2.opened = true
+		}
+		// END DATEPICKER
+
 		// Functions
 		function createTask() {
 			// Send data to Task Service
 			TaskService.addToTaskList('me', vm.newTask)
 			// Reset model
 			vm.newTask = {}
+			vm.today()
 		}
 	}
 
--- frontend/scripts/config/init.js
+++ frontend/scripts/config/init.js
@@ -1,3 +1,3 @@
 (function () {
-	angular.module('myApp', ['ui.router'])
+	angular.module('myApp', ['ui.router','ui.bootstrap'])
 })()
