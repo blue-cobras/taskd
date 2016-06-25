3b93118709acd43db807dfbcfda35ad0ddc95403
McSkwid <zmixon91@gmail.com>
2016-06-25 15:35:57 -0600

    pretty up the gulp file and add build-front-end task

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ const sass = require('gulp-sass')
 
 gulp.task('default', ['build', 'build-front-end', 'watch'])
 
+/////////////////////
+// Back End Tasks  //
+/////////////////////
+
+// Build Back-End
 gulp.task('build', () =>
     gulp.src('src/**/*.js')
         .pipe(babel({
@@ -14,32 +19,7 @@ gulp.task('build', () =>
         .pipe(gulp.dest('dist'))
 )
 
-gulp.task('build-front-end', () =>
-    gulp.src('frontend/**/*')
-        .pipe(gulp.dest('dist/public'))
-)
-
-gulp.task('browserSync', function () {
-    browserSync.init({
-        server: {
-            baseDir: 'frontend'
-        }
-    })
-})
-
-gulp.task('sass', () => {
-    gulp.src('frontend/assets/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('frontend/assets/css/'))
-        .pipe(browserSync.stream())
-})
-
-gulp.task('watch-front-end', ['browserSync', 'sass'], () => {
-    gulp.watch('frontend/assets/scss/**/*.scss', ['sass'])
-    gulp.watch('frontend/**/*.html', browserSync.reload)
-    gulp.watch('frontend/**/*.js', browserSync.reload)
-})
-
+// Watch Back-End
 gulp.task('watch', ['build', 'build-front-end'], () =>
     nodemon({
         script: 'dist/app.js',
@@ -48,3 +28,52 @@ gulp.task('watch', ['build', 'build-front-end'], () =>
         tasks: ['build']
     })
 )
+
+///////////////////////
+//  Front End Tasks  //
+///////////////////////
+
+// Build Front-End
+gulp.task('build-front-end', [
+    'bfe-css',
+    'bfe-img',
+    'bfe-js',
+    'bfe-html'
+], () => { })
+gulp.task('bfe-css', ['sass'], () =>
+    gulp.src('frontend/**/*.css')
+        .pipe(gulp.dest('dist/public'))
+)
+gulp.task('bfe-img', () =>
+    gulp.src('frontend/assets/img/**/*')
+        .pipe(gulp.dest('dist/public/assets/img'))
+)
+gulp.task('bfe-js', () =>
+    gulp.src('frontend/**/*.js')
+        .pipe(gulp.dest('dist/public'))
+)
+gulp.task('bfe-html', () =>
+    gulp.src('frontend/**/*.html')
+        .pipe(gulp.dest('dist/public'))
+)
+// Start BrowserSync
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'frontend'
+        }
+    })
+})
+// Compile SASS
+gulp.task('sass', () => {
+    gulp.src('frontend/assets/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('frontend/assets/css/'))
+        .pipe(browserSync.stream())
+})
+// Watch Front-End **NOTE: served from frontend/ not dist/
+gulp.task('watch-front-end', ['browserSync', 'sass'], () => {
+    gulp.watch('frontend/assets/scss/**/*.scss', ['sass'])
+    gulp.watch('frontend/**/*.html', browserSync.reload)
+    gulp.watch('frontend/**/*.js', browserSync.reload)
+})
